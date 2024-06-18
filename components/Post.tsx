616f1d371606cf1433027828373a20091d0502bd
rohitsingh93300 <rohitsingh93300@gmail.com>
2024-06-18 23:42:19 +0530

    added dropdown menu

--- components/Post.tsx
+++ components/Post.tsx
@@ -3,44 +3,100 @@ import React from 'react'
 import ProfilePhoto from './shared/ProfilePhoto'
 import { useUser } from '@clerk/nextjs'
 import { Button } from './ui/button'
-import { Trash2 } from 'lucide-react'
+import { Bookmark, CreditCard, Edit, EllipsisVertical, Keyboard, Save, SaveIcon, Settings, Trash2, User } from 'lucide-react'
 import { Badge } from './ui/badge'
 import PostContent from './PostContent'
 import SocialOptions from './SocialOptions'
 import { IPostDocument } from '@/models/post.model'
 import ReactTimeago from 'react-timeago'
+import { deletePostAction } from '@/lib/serveractions'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
-const Post = ({post}:{post:IPostDocument}) => {
+const Post = ({ post }: { post: IPostDocument }) => {
     const user = useUser()
-     console.log("Rohit",post)
-    //  console.log("pohit", user)
+    //  console.log("Rohit",post)
+    console.log("pohit", user)
     const fullName = post?.user?.firstName + " " + post?.user?.lastName;
     const userName = (post?.user?.firstName + post?.user?.lastName).toLowerCase();
+    const loggedInUser = user?.user?.id === post?.user?.userId;
 
     // const userName = user?.user?.primaryEmailAddress?.emailAddress.split('@')[0]
 
     return (
         <div className='bg-white my-2 mx-2 md:mx-0 rounded-lg border border-gray-300'>
             <div className='flex gap-2 p-4'>
-                <ProfilePhoto src={post?.user?.profilePhoto!}/>
+                <ProfilePhoto src={post?.user?.profilePhoto!} />
                 <div className='flex items-center justify-between w-full'>
                     <div>
                         <h1 className='text-sm font-bold'>{fullName} <Badge variant={'secondary'} className='ml-2'>You</Badge></h1>
                         <p className='text-xs text-gray-500'>@{userName}</p>
-                      
+
                         <p className='text-xs text-gray-500'>
-                            <ReactTimeago date={new Date(post.createdAt)}/>
+                            <ReactTimeago date={new Date(post.createdAt)} />
                         </p>
                     </div>
                 </div>
                 <div>
-                    <Button className='rounded-full ' size={'icon'} variant={'outline'}>
-                        <Trash2 />
-                    </Button>
+                    {
+                        loggedInUser && (
+
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+
+                                    <Button size={'icon'} className='rounded-full' variant={'outline'}>
+                                        <EllipsisVertical />
+                                    </Button>
+
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className="w-40 mr-2 md:mr-0">
+                                    {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+                                    {/* <DropdownMenuSeparator /> */}
+                                    <DropdownMenuGroup>
+                                        <DropdownMenuItem>
+                                            {/* <User className="mr-2 h-4 w-4" /> */}
+                                            <span>Profile</span>
+                                            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <Bookmark className="mr-2 h-4 w-4" />
+                                            <span>Save</span>
+                                            {/* <DropdownMenuShortcut>⌘B</DropdownMenuShortcut> */}
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <Edit className="mr-2 h-4 w-4" />
+                                            <span>Edit</span>
+                                            {/* <DropdownMenuShortcut>⌘B</DropdownMenuShortcut> */}
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <Settings className="mr-2 h-4 w-4" />
+                                            <span>Settings</span>
+                                            {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <Trash2 className="mr-2 h-4 w-4" />
+                                            <span onClick={() => {
+                                                const res = deletePostAction(post._id);
+                                            }}>Delete Post</span>
+                                            {/* <DropdownMenuShortcut>⌘K</DropdownMenuShortcut> */}
+                                        </DropdownMenuItem>
+                                    </DropdownMenuGroup>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        )
+                    }
                 </div>
             </div>
-            <PostContent post={post}/>
-            <SocialOptions/>
+            <PostContent post={post} />
+            <SocialOptions />
         </div>
     )
 }
--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -3,45 +3,45 @@
 import { Post } from "@/models/post.model";
 import { IUser } from "@/models/user.model";
 import { currentUser } from "@clerk/nextjs/server"
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import connectDB from "./db";
 import { revalidatePath } from "next/cache";
 
 // Configuration
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET // Click 'View Credentials' below to copy your API secret
 });
 
 // creating post using server actions
-export const createPostAction = async (inputText:string, selectedFile:string) => {
+export const createPostAction = async (inputText: string, selectedFile: string) => {
     await connectDB()
     const user = await currentUser()
-     
-    if(!user) throw new Error('User not authenticated');
-    if(!inputText) throw new Error('Input field is required');
+
+    if (!user) throw new Error('User not authenticated');
+    if (!inputText) throw new Error('Input field is required');
 
     const image = selectedFile;
     const userDatabase: IUser = {
-        firstName:user.firstName || 'Rohit',
-        lastName:user.lastName || 'singh',
-        userId:user.id,
+        firstName: user.firstName || 'Rohit',
+        lastName: user.lastName || 'singh',
+        userId: user.id,
         profilePhoto: user.imageUrl
     }
     // console.log("rohituser", user)
     let uploadResponse;
 
     try {
-        if(image){
+        if (image) {
             //1. create post with image
             uploadResponse = await cloudinary.uploader.upload(image);
             await Post.create({
-                description:inputText,
+                description: inputText,
                 user: userDatabase,
                 imageUrl: uploadResponse?.secure_url
             })
-        }else{
+        } else {
             //2. create post with text only
             await Post.create({
                 description: inputText,
@@ -49,7 +49,7 @@ export const createPostAction = async (inputText:string, selectedFile:string) =>
             })
         }
         revalidatePath("/")
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error)
     }
 }
@@ -58,11 +58,31 @@ export const createPostAction = async (inputText:string, selectedFile:string) =>
 export const getAllPosts = async () => {
     await connectDB()
     try {
-        const posts = await Post.find().sort({createdAt: -1});
+        const posts = await Post.find().sort({ createdAt: -1 });
         return JSON.parse(JSON.stringify(posts));
-        
+
     } catch (error) {
         console.log(error);
-        
+
+    }
+}
+
+//delete post by Id
+export const deletePostAction = async (postId: unknown) => {
+    await connectDB();
+    const user = await currentUser();
+    if (!user) throw new Error('User not authenticated')
+    const post = await Post.findById(postId);
+    if (!post) throw new Error('Post not found')
+
+    //sirf apna post delete kr payenge
+    if (post.user.userId !== user.id) {
+        throw new Error('You are not an owner of this post')
+    }
+    try {
+        await Post.deleteOne({ _id: postId })
+        revalidatePath('/')
+    } catch (error: any) {
+        throw new Error('An error occured', error)
     }
 }
