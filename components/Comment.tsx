987045efa88f1101864b0ab50d2563b7bc490a42
rohitsingh93300 <rohitsingh93300@gmail.com>
2024-06-19 21:48:01 +0530

    fixed bugs

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -9,7 +9,7 @@ const Comment = ({comment}:{comment:ICommentDocument}) => {
        <div className='mt-2'>
         <ProfilePhoto src={comment?.user?.profilePhoto!}/>
        </div>
-       <div className='flex flex-1 justify-between p-3 bg-[#F2F2F2]'>
+       <div className='flex flex-1 justify-between p-3 rounded-lg bg-[#F2F2F2]'>
         <div>
             <h1 className='text-sm font-medium'>{`${comment.user.firstName} ${comment.user.lastName}`}</h1>
             <p className='text-xm text-gray-500'>@{comment.user.firstName}</p>
